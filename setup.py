15db6930b9def510d9b05495a0d52235e48c8e76
Bartosz Gawin <bartex.gw@gmail.com>
2023-08-24 14:10:40 +0100

    init

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    description = f.read()
+
+
+setup(
+    name='NeuralNetworksTrainingPackage',
+    version='1.0.0',
+    description='Set of scripts for automated training of models from non-homogenous-paper',
+    long_description=description,
+    url='https://github.com/Bartosz-G/NeuralNetworksTrainingPackage.git',
+    author='BartoszGawin',
+    author_email='gawinbartosz@icloud.com',
+    packages=find_packages(where='NNTraining'),
+    package_dir={'': 'NNTraining'},
+    zip_safe=False,
+    install_requires=['numpy', 'pandas', 'scikit-learn', 'torch'])
